27bc42b77061e9c24ff88da72f9932508f3440a2
Nicholas Gangloff <ngangloff@smu.edu>
2014-10-23 21:06:25 -0500

    some basic starting files

--- Web/index.php
+++ Web/index.php
@@ -1,1 +1,28 @@
-Test
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta content="" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <title>IntraSMUrals</title>
+
+    <!-- JavaScript Files -->
+   
+    <!-- CSS Files -->
+    <link href="css/navbar.css" rel="stylesheet" type="text/css">
+    <link href="css/home.css" rel="stylesheet" type="text/css">
+     <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <?php include "components/navbar.html" ?>
+    <div id="homeInfo" class="sectionS">
+        <form id="burgerForm">
+            <h2 id="intro"> IntraSMUrals!!</h2>
+            
+        </form>
+    </div>
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+    
+</body>
+</html>
