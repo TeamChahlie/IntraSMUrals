37a3f27c2a5a9af2ae94d67a6c4c52e307a0d4d4
Nicholas Gangloff <ngangloff@smu.edu>
2014-11-02 18:27:25 -0600

    initial team info page

--- Web/teams.php
+++ Web/teams.php
@@ -20,7 +20,42 @@
     <?php include "components/navbar.html" ?>
 
     <div class="section">
-        <h2 id="intro"> IntraSMUrals!!</h2>
+        <h1 id="intro"> IntraSMUrals!!</h1>
+        <p id="teamsIntro"> Here is a list of the 2014-2015 SMU IntraSMUral teams!!</p>
     </div>
+
+    <TABLE id="teamTable" class="section" BORDER="5"    WIDTH="50%"   CELLPADDING="4" CELLSPACING="3">
+   <TR>
+      <TH COLSPAN="2"><BR><H3>Current Teams</H3>
+      </TH>
+   </TR>
+   <TR>
+      <TH>Team Name</TH>
+      <TH>Captain Name</TH>
+   </TR>
+   <TR ALIGN="CENTER">
+      <TD id="teamName"></TD>
+      <TD>Captain Name</TD>
+   </TR>
+   <TR ALIGN="CENTER">
+      <TD id="teamName"></TD>
+      <TD>Captain Name</TD>
+   </TR>
+   <TR ALIGN="CENTER">
+      <TD id="teamName"></TD>
+      <TD>Captain Name</TD>
+   </TR>
+   <TR ALIGN="CENTER">
+      <TD id="teamName"></TD>
+      <TD>Captain Name</TD>
+   </TR>
+   <TR ALIGN="CENTER">
+      <TD id="teamName"></TD>
+      <TD>Captain Name</TD>
+   </TR>
+   
+</TABLE>
+
+
 </body>
 </html>
