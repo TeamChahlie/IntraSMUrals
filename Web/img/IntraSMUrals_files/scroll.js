2a6aa15b3a9932dd0e37415d0464664bcffe9a09
charliealbright <albrightc25@gmail.com>
2014-11-04 15:14:05 -0600

    bug fix for team homepage

--- Web/img/IntraSMUrals_files/scroll.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var a = document.body;
-    var e = document.documentElement;
-    $(window).unbind("scroll").scroll(function () {
-        a.style.backgroundPosition = "0px " + (Math.max(e.scrollTop, a.scrollTop) / 4) + "px";
-    });
-});
