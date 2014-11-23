24d4fb7837286ec2c42c9ac3ff7f8bf2e5d4e732
charliealbright <albrightc25@gmail.com>
2014-11-23 17:47:41 -0600

    adding new GUI stuff

--- Web/editSport.php
+++ Web/editSport.php
@@ -29,13 +29,9 @@
     <div class="section">
         <h2 class="pageHeader">Sport Editor</h2>
         <h2 class="subHeader"></h2>
-        <div id="sportTeamList">
-            <h2 class="sectionHeader">Teams</h2>
-            <p id="noTeams" class="hidden">There are no teams affiliated with this sport yet</p>
-        </div>
+
         <div id="sportSchedule">
             <h2 class="sectionHeader">Upcoming Games</h2>
-            <p class="noContent">Select a Team on the right to view upcoming games</p>
 
             <!--            <div class="scheduleEvent">-->
             <!--                <span class="eventDate">Wednesday, Oct. 24th</span>-->
@@ -46,7 +42,11 @@
             <!--            </div>-->
 
         </div>
+        <div id="sportTeamList">
+            <h2 class="sectionHeader">Teams</h2>
+            <p id="noTeams" class="hidden">There are no teams affiliated with this sport yet</p>
 
+        </div>
     </div>
 </div>
 </body>
--- Web/js/editSport.js
+++ Web/js/editSport.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.subHeader').text(get('sportName'));
 
     getTeams();
+    addTeamHoverListeners();
 });
 
 function get(name){
@@ -10,4 +11,54 @@ function get(name){
 }
 
 function getTeams() {
+    $.getJSON('api/getTeamsInSport/' + get('sportName'), function(teams) {
+
+        if(teams.length == 0) {
+            document.getElementById('noTeams').className = "visible";
+        } else {
+            for(var team in teams) {
+                var teamName = teams[team].teamName;
+                var teamList = document.getElementById('sportTeamList');
+
+                var div = document.createElement('div');
+                div.className = "teamEditorButton";
+                teamList.appendChild(div);
+
+                var subDiv1 = document.createElement('div');
+                subDiv1.className = "teamDefault";
+                div.appendChild(subDiv1);
+
+                var h2 = document.createElement('h2');
+                h2.className = "teamTitle";
+                h2.textContent = teamName;
+                subDiv1.appendChild(h2);
+
+                var subDiv2 = document.createElement('div');
+                subDiv2.className = "teamOptionMenu";
+                div.appendChild(subDiv2);
+
+                var editDiv = document.createElement('div');
+                editDiv.className = "teamOptionButton editTeam";
+                editDiv.textContent = "Edit";
+                subDiv2.appendChild(editDiv);
+
+                var deleteDiv = document.createElement('div');
+                deleteDiv.className = "teamOptionButton deleteTeam";
+                deleteDiv.textContent = "Delete";
+                subDiv2.appendChild(deleteDiv);
+
+            }
+        }
+    });
+}
+
+function addTeamHoverListeners() {
+    $('#sportTeamList').on('mouseenter', '.teamEditorButton', function() {
+        $(this).children('.teamDefault').animate({width: 'toggle'}, 200);
+        $(this).children('.teamOptionMenu').animate({width: 'toggle'}, 300);
+    });
+    $('#sportTeamList').on('mouseleave', '.teamEditorButton', function() {
+        $(this).children('.teamOptionMenu').animate({width: 'toggle'}, 200);
+        $(this).children('.teamDefault').animate({width: 'toggle'}, 300);
+    });
 }
