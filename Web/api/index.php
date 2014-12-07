58a6c388ae4db01f26aa8366463f83109ad39287
Hector Curi <hcuri@me.com>
2014-12-06 21:09:30 -0600

    Add insertStudent and deleteStudent
    Example POST Format:
    {"teamID": 1,
     "studentID": 12345679}

--- Web/api/index.php
+++ Web/api/index.php
@@ -45,6 +45,8 @@ $app->post('/updateMatchScore', 'updateMatchScore');
 
 //Team level
 $app->get('/getStudentsInTeam/:teamID', 'getStudentsInTeam');
+$app->post('/insertStudent', 'insertStudent');
+$app->post('/deleteStudent', 'deleteStudent');
 
 //-----ADMIN Calls, please don't move anything between here and the previous comment
 
@@ -355,6 +357,44 @@ function getStudentsInTeam($teamID) {
         echo '{"error":{"text":' . $e->getMessage() . '}}';
     }
 }
+
+// inserts student into a team using the teamID and studentID
+function insertStudent() {
+    $sql = "INSERT INTO Involvement (teamID, studentID) VALUES (:teamID, :studentID)";
+    $app = \Slim\Slim::getInstance();
+    $request = $app->request();
+    $info = json_decode($request->getBody());
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("teamID", $info->teamID);
+        $stmt->bindParam("studentID", $info->studentID);
+        $stmt->execute();
+        echo '{"success": true}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text":' . $e->getMessage() . '}}';
+    }
+}
+
+// removes student from a team using teamID and studentID
+function deleteStudent() {
+    $sql = "DELETE FROM Involvement WHERE teamID = :teamID AND studentID = :studentID";
+    $app = \Slim\Slim::getInstance();
+    $request = $app->request();
+    $info = json_decode($request->getBody());
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("teamID", $info->teamID);
+        $stmt->bindParam("studentID", $info->studentID);
+        $stmt->execute();
+        echo '{"success": true}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text":' . $e->getMessage() . '}}';
+    }
+}
+
+//============================== END OF ADMIN ==============================//
 
 //Inserting a student into the database (no involvement added at this time)
 function insertCaptain() {
@@ -964,10 +1004,10 @@ function getUpcomingMatches() {
 }
 
 function getConnection() {
-//    $dbhost = "127.0.0.1";
-//    $dbpass = "";
-    $dbhost = "localhost";
-    $dbpass = "root";
+   $dbhost = "127.0.0.1";
+   $dbpass = "";
+    // $dbhost = "localhost";
+    // $dbpass = "root";
     $dbuser = "root";
     $dbname = "IntraSMUrals";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
